d2dedcf04633ca6953d99925a77924c3e938c979
miroslaw-myjak <miroslawtmyjak@gmail.com>
2024-03-15 15:10:47 +0000

    add NSG

--- nsg.tf
+++ nsg.tf
@@ -14,7 +14,8 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         source_address_prefix = "*"
         destination_address_prefix = "*"
 
-        tags = var.tags
-
     }
+    
+    tags = var.tags
+
 }
