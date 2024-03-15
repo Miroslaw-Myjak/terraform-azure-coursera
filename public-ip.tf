b63280daacd653e0c2d62257a952b49644b2a6f1
miroslaw-myjak <miroslawtmyjak@gmail.com>
2024-03-15 14:57:19 +0000

    add Ip address

--- public-ip.tf
+++ public-ip.tf
@@ -1,9 +1,9 @@
-resource "azurerm_public_ip" "" {
+resource "azurerm_public_ip" "myterraformpublicipaddress" {
     name = var.ipaddressname
     resource_group_name = azurerm_resource_group.myTerraformGroup.name
     location = var.location
     allocation_method = "Dynamic"
 
     tags = var.tags
-    
+
 }
