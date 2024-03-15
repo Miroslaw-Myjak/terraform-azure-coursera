2e46d1ba6525d708192db165f2d4f85726c1f028
miroslaw-myjak <miroslawtmyjak@gmail.com>
2024-03-15 13:21:17 +0000

    fix provider source

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 terraform {
     required_providers {
       azurerm = {
-          source = "harshicorp/azurerm"
+          source = "hashicorp/azurerm"
           version = "=2.56.0"
       }
     }   
