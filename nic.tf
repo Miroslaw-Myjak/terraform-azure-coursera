e113b93bd9ec065d60192a2bbd2ef3cfeafd57bd
miroslaw-myjak <miroslawtmyjak@gmail.com>
2024-03-15 15:21:06 +0000

    add Nic

---
+++ nic.tf
@@ -0,0 +1,19 @@
+resource "azurerm_network_interface" "myterraformnic" {
+  name                = var.nicname
+  location            = var.location
+  resource_group_name = azurerm_resource_group.myTerraformGroup.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.myterraformsubnet.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id =  azurerm_public_ip.myterraformpublicipaddress.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.myterraformnic.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
